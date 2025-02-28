6d9b2e4d17c2601615856eeab83b70295f19fae4
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-02-28 00:11:08 -0500

    adding new styles for tailwind

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -3,9 +3,9 @@ import { Header } from "../components/Header.jsx"
 
 export const Layout = () => {
   return (
-    <>
+    <div className="bg-gray-900">
       <Header />
       <Outlet />
-    </>
+    </div>
   )
 }
--- frontend/src/layouts/Section.jsx
+++ frontend/src/layouts/Section.jsx
@@ -1,14 +1,7 @@
-import PropTypes from "prop-types"
-
 export const Section = ({ children, className }) => {
   return (
     <section className={`px-4 flex flex-col w-full lg:w-[920px] h-dvh items-center justify-center ${className || ""}`}>
       {children}
     </section>
   )
-}
-
-Section.propTypes = {
-  children: PropTypes.element.isRequired,
-  className: PropTypes.string
 }
